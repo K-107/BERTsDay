8ff3ae881a0ea0751d68dc230ada505f437681d6
ppkk0906 <59152690+ppkk0906@users.noreply.github.com>
2021-04-14 14:36:53 +0900

    코멘트 수정

--- Bert_fine_tuning/inference.py
+++ Bert_fine_tuning/inference.py
@@ -1,8 +1,6 @@
 # -*- coding: utf-8 -*-
 
-############################### TODO ##########################################
 # 필요한 모듈 불러오기
-###############################################################################
 
 import argparse
 import os
@@ -40,9 +38,7 @@ config = tf.ConfigProto(intra_op_parallelism_threads=1,
 sess = tf.compat.v1.Session(config=config)
 
 if type_ == 'bert':
-############################### TODO 경로 고치기 ##########################################
     bert_model_hub_path = args.bertpath
-###########################################################################################
     is_bert = True
 elif type_ == 'albert':
     bert_model_hub_path = 'https://tfhub.dev/google/albert_base/1'
@@ -51,9 +47,7 @@ else:
     raise ValueError('type must be one of these values: %s' % str(VALID_TYPES))
 
 
-############################### TODO ##########################################
 # 모델과 벡터라이저 불러오기
-###############################################################################
 vocab_file = os.path.join(bert_model_hub_path, "assets/vocab.korean.rawtext.list")
 bert_to_array = BERTToArray(is_bert, vocab_file)
 
@@ -90,40 +84,6 @@ while True:
         
     if input_text == 'quit':
         break
-
-
-
-############################### TODO ##########################################
-# 사용자가 입력한 한 문장을 슬롯태깅 모델에 넣어서 결과 뽑아내기
-###############################################################################
-
-"""
-def get_results(input_ids, input_mask, segment_ids, tags_arr, tags_to_array):
-    inferred_tags, slots_score = model.predict_slots([data_input_ids, data_input_mask, data_segment_ids],
-                                                    tags_to_array)
-
-    gold_tags = [x.split() for x in tags_arr]
-
-    f1_score = metrics.f1_score(flatten(gold_tags), flatten(inferred_tags), average='micro')
-
-    tag_incorrect = ''
-    for i, sent in enumerate(input_ids):
-        if inferred_tags[i] != gold_tags[i]:
-            tokens = bert_to_array.tokenizer.convert_ids_to_tokens(input_ids[i])
-            tag_incorrect += 'sent {}\n'.format(tokens)
-            tag_incorrect += ('pred: {}\n'.format(inferred_tags[i]))
-            tag_incorrect += ('score: {}\n'.format(slots_score[i]))
-            tag_incorrect += ('ansr: {}\n\n'.format(gold_tags[i]))
-
-
-    return f1_score, tag_incorrect
-
-
-f1_score, tag_incorrect = get_results(data_input_ids, data_input_mask, data_segment_ids,
-                                                            data_tags_arr, tags_to_array)
-"""
-
-i
 
 tf.compat.v1.reset_default_graph()
 
