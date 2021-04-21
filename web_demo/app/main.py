80d90b1b943513cf4f5fa9a5ae61599ac5bea121
ppkk0906 <59152690+ppkk0906@users.noreply.github.com>
2021-04-21 12:14:53 +0900

    일단 작동은 함

--- web_demo/app/main.py
+++ web_demo/app/main.py
@@ -4,6 +4,8 @@ import os
 import sys
 import pickle
 import tensorflow as tf
+import datetime
+import numpy as np
 sys.path.append(os.path.join(os.getcwd(), "Bert_fine_tuning"))
 from to_array.bert_to_array import BERTToArray
 from models.bert_slot_model import BertSlotModel
@@ -42,13 +44,19 @@ app.template_folder = "web_demo/app/templates"
 
 @app.route("/")
 def home():
-
 ############################### TODO ##########################################
 # 슬롯 사전 만들기
-    app.slot_dict = {'start': None, 'end': None, 'date':None, 'num':None ,'name': None, 'phone': None}
+    app.slot_dict = {'start': '', 'end': '', 'date': '', 'person': '', 'name': '', 'phone': ''}
+    app.filled_num = 0
 
     return render_template("index.html")
 
+answer_name_arr = ['성함이 어떻게 되시나요?', '이름을 말해주세요.']
+answer_phone_arr = ['연락 가능한 번호를 써주세요.(예시 : 010-1234-1234)', '전화번호를 알려주세요.(예시 : 010-1234-1234)', '예약자 분의 번호를 입력해주세요.(예시 : 010-1234-1234)']
+answer_date_arr = ['몇 월 며칠에 예약하고 싶으신가요?', '예약하고 싶은 월일을 입력해주세요. (예시: 1월 3일)', '예약하시려는 날짜를 알려주세요.']
+answer_start_arr = ['몇 시로 예약하실 건가요?', '몇 시부터 사용하실 건가요?', '사용 시작 시간을 알려주세요.']
+answer_end_arr = ['몇 시까지 이용하실 건가요?', '언제까지 사용하실 건가요?', '종료 시간을 알려주세요.']
+answer_person_arr = ['총 몇 명이신가요?', '몇 명이서 쓰실 건가요?', '이용 인원을 말씀해주세요?']
 
 @app.route("/get")
 def get_bot_response():
@@ -56,7 +64,7 @@ def get_bot_response():
 
     #벡터화
     input_text = ' '.join(tokenizer.tokenize(userText))
-    tokens = input_text.split()
+    token_list = input_text.split()
     data_text_arr = [input_text]
     data_input_ids, data_input_mask, data_segment_ids = bert_to_array.transform(data_text_arr)
     
@@ -64,16 +72,64 @@ def get_bot_response():
     with graph.as_default():
         with sess.as_default():
             inferred_tags, slots_score = model.predict_slots([data_input_ids, data_input_mask, data_segment_ids], tags_to_array)
-
-############################### TODO ##########################################
-# 1. 사용자가 입력한 한 문장을 슬롯태깅 모델에 넣어서 결과 뽑아내기
-# 2. 추출된 슬롯 정보를 가지고 더 필요한 정보 물어보는 규칙 만들기 (if문)
+    
+    today = datetime.datetime.now()
+    if '오늘' in userText:
+        app.slot_dict['date'] = str(today.month) + "월" + str(today.day) + "일"
+    if '내일' in userText:
+        app.slot_dict['date'] = str(today.month) + "월" + str(today.day + 1) + "일"
+    
     try:
-        app.slot_dict['start'] = ''
-        print(app.slot_dict)
+        # 1. 사용자가 입력한 한 문장을 슬롯태깅 모델에 넣어서 결과 뽑아내기
+        for i in range(0,len(inferred_tags[0])):
+            if inferred_tags[0][i]=='날짜':
+                if app.slot_dict['date'] == "": app.filled_num += 1
+                app.slot_dict['date'] += token_list[i]     
+            elif inferred_tags[0][i]=='시작시간':
+                if app.slot_dict['start'] == "": app.filled_num += 1
+                app.slot_dict['start'] += token_list[i]     
+            elif inferred_tags[0][i]=='종료시간':
+                if app.slot_dict['end'] == "": app.filled_num += 1
+                app.slot_dict['end'] += token_list[i]     
+            elif inferred_tags[0][i]=='인원':
+                if app.slot_dict['person'] == "": app.filled_num += 1
+                app.slot_dict['person'] += token_list[i] 
+            elif inferred_tags[0][i]=='이름':
+                if app.slot_dict['name'] == "": app.filled_num += 1
+                app.slot_dict['name'] += token_list[i]
+            elif inferred_tags[0][i]=='번호':
+                if app.slot_dict['phone'] == "": app.filled_num += 1
+                app.slot_dict['phone'] += token_list[i]   
+
+        print(f"slot_dict: {app.slot_dict}<br>input_text: {token_list}<br>inferred_tags: {inferred_tags} <br>slots_score: {slots_score}")
+
+        # 2. 추출된 슬롯 정보를 가지고 더 필요한 정보 물어보는 규칙 만들기 (if문)
+        if ((app.slot_dict['start'] != "") and (app.slot_dict['end'] != "") and (app.slot_dict['person'] != "")and (app.slot_dict['date'] != "") and (app.slot_dict['name'] != "") and (app.slot_dict['phone'] != "")):
+            return '예약이 완료되었습니다. 예약을 종료합니다.'
+    
+        elif ((app.slot_dict['start'] == "") and (app.slot_dict['end'] == "") and (app.slot_dict['person'] == "") and (app.slot_dict['date'] == "") and (app.slot_dict['name'] == "") and (app.slot_dict['phone'] == "")):
+            return '죄송합니다 제가 이해를 잘 못해서 다시 한번 입력해주세요.'
+
+        else:
+            if app.debug:
+                response = f"<br><br>slot_dict: {app.slot_dict}<br>input_text: {token_list}<br>inferred_tags: {inferred_tags} <br>slots_score: {slots_score}"
+            else:
+                response = ""
+            if app.slot_dict['date'] == '':
+                return str(np.random.choice(answer_date_arr, 1)[0])+ response
+            elif app.slot_dict['start'] == '':
+                return str(np.random.choice(answer_start_arr, 1)[0]) + response
+            elif app.slot_dict['end'] == '':
+                return str(np.random.choice(answer_end_arr, 1)[0]) + response
+            elif app.slot_dict['person'] == '':
+                return str(np.random.choice(answer_person_arr, 1)[0]) + response
+            elif app.slot_dict['name'] == '':
+                return str(np.random.choice(answer_name_arr, 1)[0]) + response
+            elif app.slot_dict['phone'] == '':
+                return str(np.random.choice(answer_phone_arr, 1)[0]) + respons
+
     except Exception as e:
+        print(e)
         return "오류가 발생했습니다, 페이지를 다시 열어주세요"
     
-
-    
-    return f"input_text: {tokens}<br>inferred_tags: {inferred_tags} <br>slots_score: {slots_score}"
+    return "이 문장은 출력될 일이 없습니다."
