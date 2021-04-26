84337eca58777c3ac2ac8f81fb1f5959232cdd98
ppkk0906 <59152690+ppkk0906@users.noreply.github.com>
2021-04-26 12:28:03 +0900

    ngrok를 위해 run.py 수정

--- web_demo/run.py
+++ web_demo/run.py
@@ -4,5 +4,6 @@ from app.main import app
 import webbrowser
 
 if __name__ == "__main__":
-    app.run("localhost",port=6006, debug=True)
+    #app.run("localhost",port=6006, debug=True) # 테스트할때는 이것으로 쓰세요
+    app.run() #ngrok 쓸 때는 이거로 쓰세요
     webbrowser.open("http://localhost:6006")
